a0b9344c4e9cc9de0dbb12633171b0e5d522072f
Jenlizarraga <56921400+Jenlizarraga@users.noreply.github.com>
2022-05-14 20:47:24 -0700

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -1,3 +1,5 @@
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -26,3 +28,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
